84489f3d3a7a01898a843aa97ffe5a204735f594
Dariusz Oskroba <darek@DarekMacBookPro.local>
2013-06-14 01:57:29 +0200

    WebSerwis

---
+++ WebSerwis/Model.js
@@ -0,0 +1,24 @@
+﻿
+guidedModel =// @startlock
+{
+	Rejestracje :
+	{
+		events :
+		{
+			onInit:function()
+			{// @endlock
+				this.DataWpisu = new Date();
+			}// @startlock
+		}
+	},
+	Ankiety :
+	{
+		events :
+		{
+			onInit:function()
+			{// @endlock
+				this.DataWpisu = new Date();
+			}// @startlock
+		}
+	}
+};// @endlock
